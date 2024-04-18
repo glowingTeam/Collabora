3c4f6f4429212a65355628670039404960311b60
ZidZaky <zzidanirfan@gmail.com>
2024-04-18 15:39:25 +0700

    test

--- resources/views/page/create.blade.php
+++ resources/views/page/create.blade.php
@@ -25,6 +25,7 @@
                     <option value="1">One</option>
                     <option value="2">Two</option>
                     <option value="3">Three</option>
+                    <option value="4">Four</option>
                 </select>
             </div>
             <div class="mb-3">
@@ -38,5 +39,5 @@
             </div>
         </form>
     </div>
-    
+
 @endsection
