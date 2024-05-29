9da801fc9eccb0fe8c440cd3a70b616ed03d72cd
unknown <aldayemima10@gmail.com>
2024-05-30 01:00:06 +0700

    new dashboard

--- resources/views/page/dashboard.blade.php
+++ resources/views/page/dashboard.blade.php
@@ -13,14 +13,36 @@
 <body>
 @extends('layouts.main')
 @section('content')
-
 <div class="slide-container">
     <div class="slide-content">
+        
+        <div class="judul2">
+            <h1><b>Hi, it's Collabora</b></h1>
+        </div>
+        <section class="container">
+            <div class="slide-wrapper">
+                <div class="slider">
+                <img id="slide-1" src="img/foto1.jpg" alt="" class="card-img">
+                <img id="slide-2" src="img/foto2.jpg" alt="" class="card-img">
+                <img id="slide-3" src="img/foto3.jpg" alt="" class="card-img">
+                </div>
+                <div class="slider-nav">
+                    <a href="#slide-1"></a>
+                    <a href="#slide-2"></a>
+                    <a href="#slide-3"></a>
+                </div>
+            </div>
+        </section>
+
+        <div class="judul1">
+                    <h2><b>List Volenteer</b></h2>
+        </div>
         <div class="card-wrapper">
             @foreach ($events as $event )
             <div class="card ">
+                
                 <div class="image-content">
-                    <span class="overlay">  </span>
+                    <span class="overlay"> </span>
 
                         <div class="card-image">
                             <img src="img/potret.png" alt="" class="card-img">
