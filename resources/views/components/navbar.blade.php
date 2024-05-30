6e7c96032f60ec29cc1915f24889a9bb5ac61b09
unknown <aldayemima10@gmail.com>
2024-05-30 14:09:53 +0700

    new2

--- resources/views/components/navbar.blade.php
+++ resources/views/components/navbar.blade.php
@@ -1,9 +1,7 @@
 <nav class="navbar navbar-expand-lg bg-body-tertiary bg-dark ">
     <div class="container-fluid ">
-      <a class="navbar-brand text-light" href="/">Collabora</a>
-      <button class="navbar-toggler" type="button" data-bs-toggle="collapse" data-bs-target="#navbarNavAltMarkup" aria-controls="navbarNavAltMarkup" aria-expanded="false" aria-label="Toggle navigation">
-        <span class="navbar-toggler-icon"></span>
-      </button>
+          <a class="navbar-brand text-light" href="/dashboard"><b>Collabora</b></a>
+    
       <div class="collapse  navbar-collapse " id="navbarNavAltMarkup">
         <div class="navbar-nav">
           <a class="nav-link text-light" aria-current="page" href="/dashboard">Dashboard</a>
@@ -21,11 +19,7 @@
               </li>
             </ul>
           </div>
-
-          <div class="navbar-nav ml-auto">
-            <a class="nav-link text-light btn btn-light text-dark"  href="/account">Login</a>
-          </div>
-          {{-- <a class="nav-link disabled" aria-disabled="true">Disabled</a> --}}
+        <!-- <a class="nav-link disabled" aria-disabled="true">Disabled</a> -->
         </div>
       </div>
     </div>
